81fc4a394cc36b70284242743312f9f03e480bfc
KatOlista^^! <olya.katyushina@gmail.com>
2024-03-08 18:25:16 +0100

    init todo list project

---
+++ public/todos.js
@@ -0,0 +1,21 @@
+export const todos = [
+  {
+    id: '1',
+    title: 'JavaScript',
+  },
+
+  {
+    id: '2',
+    title: 'TypeScript',
+  },
+
+  {
+    id: '3',
+    title: 'SCSS',
+  },
+
+  {
+    id: '4',
+    title: 'HTML',
+  },
+];
